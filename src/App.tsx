2ec9cac62b0785f486e911460c998393b4cde293
nvhoan11092003 <nvhoan11092003@gmail.com>
2023-07-02 07:39:47 +0700

    tailwindcss + base router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
-    ""
+    <BrowserRouter>
+      <Routes>
+        {/*   client */}
+        <Route path=""  >
+
+        </Route>
+        {/* admin  */}
+        <Route path=""  ></Route>
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
