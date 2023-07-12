bbed181a1c13a530792bb3bae718f75ff9787622
minkiv <92282454+minkiv@users.noreply.github.com>
2023-07-12 16:01:31 +0700

    Client Layouts Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WebsiteLayouts from "./Layouts/websiteLayouts";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/clientPages/HomePage";
+import AboutPage from "./pages/clientPages/AboutPage";
+import ServicePage from "./pages/clientPages/ServicePage";
+import MenuPage from "./pages/clientPages/MenuPage";
+import ContactPage from "./pages/clientPages/ContactPage";
+import BookingPage from "./pages/clientPages/BookingPage";
+import TeamPage from "./pages/clientPages/TeamPage";
 
 function App() {
   return (
@@ -9,6 +15,12 @@ function App() {
         {/*   client */}
         <Route path="/" element={<WebsiteLayouts />}>
           <Route index element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/service" element={<ServicePage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/booking" element={<BookingPage />} />
+          <Route path="/team" element={<TeamPage />} />
         </Route>
         {/* admin  */}
         <Route path=""></Route>
