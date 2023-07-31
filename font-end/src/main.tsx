66b058aac460ae2ada39ca9c2dc77c4d546c54c5
nvhoan11092003 <nvhoan11092003@gmail.com>
2023-07-31 08:33:00 +0700

    router update

--- font-end/src/main.tsx
+++ font-end/src/main.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
