401889ce7217a577f2b9003346d10373eab3244d
cxm <manhcx1111@gmail.com>
2023-08-03 20:31:47 +0700

    getAllUser

--- back-end/src/router/user.js
+++ back-end/src/router/user.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { changePassword, forgotPassword, signin, signup } from "../controller/user"
+import { changePassword, forgotPassword, getAllUser, signin, signup } from "../controller/user"
 
 const router = express.Router()
 router.post("/signup",signup)
 router.post("/signin",signin)
 router.post("/forgot-password",forgotPassword)
 router.post("/change-password",changePassword)
-
+router.get("/user",getAllUser)
 export default router
